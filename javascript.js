a93816c7b24cc1487d9934a992be8942e9a3082e
jimboscatfood <149596389+jimboscatfood@users.noreply.github.com>
2024-06-30 23:50:01 +0800

    Create a new function named playGame
    The new function playGame is to call the playRound function 5 times
    so 5 rounds will be played to determine the final winner
    
    The playRound function is moved and called inside the playGame function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     //Write code to randomly return string values of
     //"rock" or "paper" or "scissors"
@@ -14,16 +11,19 @@ function getComputerChoice() {
     //IF the random number equals 1 THEN
         //RETURN "rock"
     if (computerChoice === 1) {
+        console.log ("The computer chose rock");
         return "rock";
     }
     //ELSE IF the random number equals 2 THEN
         //RETURN "paper"
     else if (computerChoice === 2) {
+        console.log ("The computer chose paper");
         return "paper";
     }
     //ELSE IF the random number equals 3 THEN
         //RETURN "scissors"
     else if (computerChoice === 3) {
+        console.log ("The computer chose scissors");
         return "scissors";
     }
 }
@@ -46,56 +46,88 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    //IF both choices are the same THEN
-    if (humanChoice == computerChoice) {
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        //IF both choices are the same THEN
+        if (humanChoice == computerChoice) {
+            //PRINT "It's a tie!"
+            console.log("It's a tie!");
+        }
+        //ELSE IF human uses rock, computer uses paper THEN
+        else if (humanChoice === "rock") {
+            if (computerChoice === "paper") {
+                //PRINT "You lose! Paper beats Rock"
+                console.log("You lose! Paper beats Rock");
+                //Increment computerScore by 1
+                computerScore++;
+            }
+            //ELSE IF human uses rock, computer uses scissors THEN
+            else {
+                //PRINT "You win! Rock beats Scissors"
+                console.log("You win! Rock beats Scissors")
+                //Increment humanScore by 1
+                humanScore++;        
+            }
+        }
+        //ELSE IF human uses paper...
+        else if (humanChoice === "paper") {
+            if (computerChoice === "scissors") {
+                console.log("You lose! Scissors beats Paper");
+                computerScore++;
+            }
+            else {
+                console.log("You win! Paper beats Rock")
+                humanScore++;
+            }
+        }
+        //ELSE IF human uses scissors...
+        else if (humanChoice === "scissors") {
+            if (computerChoice === "rock") {
+                console.log("You lose! Rock beats Scissors");
+                computerScore++;
+            }
+            else {
+                console.log("You win! Scissors beats Paper")
+                humanScore++;
+            }
+        }
+    }
+    //End of playRound function
+    
+    //FOR no. of round < 5
+    for (let roundNumber = 0; roundNumber < 5; roundNumber++) {
+        //CALL the two functions to get human and computer choice before playing round
+        //STORE the two values from the functions into two variables
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        //CALL playRound function with the two parameters which calls the get choice functions
+        playRound (humanSelection, computerSelection );
+        //PRINT score
+        console.log("Your score is: " + humanScore, '\n',"Computer's score is: " + computerScore);
+    }
+    
+    //END FOR
+
+    //PRINT score
+    console.log("Your final score is: " + humanScore, '\n',"Computer's final score is: " + computerScore)
+    //IF human score is higher than computer THEN
+    if (humanScore > computerScore) {
+        //PRINT "You're the winner!"
+        console.log("You're the winner!");
+    }
+    //ELSE IF computer score is higher
+    else if (humanScore < computerScore) {
+        //PRINT "Computer is the winner!"
+        console.log("Computer is the winner!");
+    }
+    //ELSE IF both scores are the same
+    else {
         //PRINT "It's a tie!"
         console.log("It's a tie!");
     }
-    //ELSE IF human uses rock, computer uses paper THEN
-    else if (humanChoice === "rock") {
-        if (computerChoice === "paper") {
-            //PRINT "You lose! Paper beats Rock"
-            console.log("You lose! Paper beats Rock");
-            //Increment computerScore by 1
-            computerScore++;
-        }
-        //ELSE IF human uses rock, computer uses scissors THEN
-        else {
-            //PRINT "You win! Rock beats Scissors"
-            console.log("You win! Rock beats Scissors")
-            //Increment humanScore by 1
-            humanScore++;        
-        }
-    }
-    //ELSE IF human uses paper...
-    else if (humanChoice === "paper") {
-        if (computerChoice === "scissors") {
-            console.log("You lose! Scissors beats Paper");
-            computerScore++;
-        }
-        else {
-            console.log("You win! Paper beats Rock")
-            humanScore++;
-        }
-    }
-    //ELSE IF human uses scissors...
-    else if (humanChoice === "scissors") {
-        if (computerChoice === "rock") {
-            console.log("You lose! Rock beats Scissors");
-            computerScore++;
-        }
-        else {
-            console.log("You win! Scissors beats Paper")
-            humanScore++;
-        }
-    }
 }
 
-
-//CALL the two functions to get human and computer choice before playing round
-//STORE the two values from the functions into two variables
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
